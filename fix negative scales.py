bc1791f1134b325a7c8a138a984f86507f29f15d
Yago Martínez <65448292+TheRealQbit@users.noreply.github.com>
2023-06-11 17:28:48 +0200

    Update fix negative scales.py

--- fix negative scales.py
+++ fix negative scales.py
@@ -1,10 +1,21 @@
 import bpy
 
 def fix_negative_scale(obj):
-    """Check and fix negative scale of an object."""
+    """Check and fix negative scale of an object while preserving its transformation."""
     scale = obj.scale
     if scale.x < 0 or scale.y < 0 or scale.z < 0:
+        # Store the object's world matrix
+        obj_matrix = obj.matrix_world.copy()
+
+        # Apply the inverse of the object's world matrix to reset its transformation
+        obj.matrix_world.invert()
+
+        # Fix negative scale by taking the absolute values of the scale components
         obj.scale = (abs(scale.x), abs(scale.y), abs(scale.z))
+
+        # Apply the original world matrix back to the object
+        obj.matrix_world = obj_matrix
+
         print(f"Fixed negative scale for object '{obj.name}'.")
 
 # Select all objects in the scene
